92de2e0b0f3705a96060f71b4396520a4ff5a991
jaimepreuss <jaimepreuss@gmail.com>
2020-08-13 22:02:02 -0700

    run build folder

---
+++ build/precache-manifest.b1b1e6690efda62df99f1764c66cb1e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a109dc7cebb2139d5f7e8dd380895707",
+    "url": "/index.html"
+  },
+  {
+    "revision": "929bc2f3df8a0453a4d6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "37616b6808fb994c29b3",
+    "url": "/static/js/2.e5b6db19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e5b6db19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "929bc2f3df8a0453a4d6",
+    "url": "/static/js/main.f25469ec.chunk.js"
+  },
+  {
+    "revision": "7e022b215600176ce10b",
+    "url": "/static/js/runtime-main.4ca8a034.js"
+  }
+]);
